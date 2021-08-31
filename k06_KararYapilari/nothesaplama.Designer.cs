dcbaa3c6f034436bc860abe4e16e62ca6da6d402
leyla <leyla0694@outlook.com>
2021-08-31 16:47:14 +0300

    not uygulaması örneğim

--- k06_KararYapilari/nothesaplama.Designer.cs
+++ k06_KararYapilari/nothesaplama.Designer.cs
@@ -29,144 +29,73 @@ namespace k06_KararYapilari
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(186, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 40);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(100, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "vize notu";
+            this.label1.Size = new System.Drawing.Size(230, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vize Notunu Giriniz";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(444, 50);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(501, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "final notu";
+            this.label2.Size = new System.Drawing.Size(235, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Final Notunu Giriniz";
             // 
-            // label3
+            // txtVize
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(223, 214);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 17);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "not ortalaması";
+            this.txtVize.Location = new System.Drawing.Point(103, 89);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(100, 22);
+            this.txtVize.TabIndex = 5;
             // 
-            // textBox2
+            // txtFinal
             // 
-            this.textBox2.Location = new System.Drawing.Point(600, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 4;
+            this.txtFinal.Location = new System.Drawing.Point(535, 89);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(100, 22);
+            this.txtFinal.TabIndex = 6;
             // 
-            // textBox3
+            // button1
             // 
-            this.textBox3.Location = new System.Drawing.Point(383, 209);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 5;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(255, 287);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 17);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "harf notu";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(383, 282);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 7;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(316, 386);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(86, 22);
-            this.textBox5.TabIndex = 8;
-            this.textBox5.Text = "geçti/kaldı";
-            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(186, 102);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 22);
-            this.textBox6.TabIndex = 9;
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(600, 102);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 22);
-            this.textBox7.TabIndex = 10;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(32, 110);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(124, 17);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "vize notu ortalama";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(424, 110);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(125, 17);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "final notu ortalama";
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(320, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 59);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Geçti/Kaldı";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // nothesaplama
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
+            this.BackColor = System.Drawing.Color.HotPink;
+            this.BackgroundImage = global::k06_KararYapilari.Properties.Resources.IMG_20190214_WA0051;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 497);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVize);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
             this.Name = "nothesaplama";
             this.Text = "nothesaplama";
             this.ResumeLayout(false);
@@ -175,19 +104,10 @@ namespace k06_KararYapilari
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Button button1;
     }
 }
--- k06_KararYapilari/nothesaplama.cs
+++ k06_KararYapilari/nothesaplama.cs
@@ -19,17 +19,65 @@ namespace k06_KararYapilari
 
         private void textBox5_TextChanged(object sender, EventArgs e)
         {
-            var vizenotu = textBox1.Text;
-         
-            var finalnotu = textBox2.Text;
 
-           
-            
+        }
 
-            if (notOrtalaması<30)
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var vizeDeger = txtVize.Text;
+            var vize = Convert.ToDouble(vizeDeger);
+
+            var finalDeger = txtFinal.Text;
+            var final = Convert.ToDouble(vizeDeger);
+
+            var ortalama = vize * 0.3 + final * 0.7;
+
+            if (ortalama >= 0 && ortalama < 30)
             {
-
+                MessageBox.Show("FF/KALDI" + $"ortalamasi:" + ortalama);
+            }
+            else if (ortalama >= 30 && ortalama < 50)
+            {
+                MessageBox.Show("DD/KALDI" + $"Ortalamasi:" + ortalama);
+            }
+            else if (ortalama >= 50 && ortalama < 60)
+            {
+                MessageBox.Show("CC/GECTI" + $"Ortalamasi:" + ortalama);
+            }
+            else if (ortalama >= 60 && ortalama < 85)
+            {
+                MessageBox.Show("BA/GECTI" + $"Ortalamasi:" + ortalama);
+            }
+            else if (ortalama > 85)
+            {
+                MessageBox.Show("AA/GECTI" + $"Ortalamasi:" + ortalama);
+            }
+            else if (ortalama >=100 )
+            {
+                MessageBox.Show("Geçerli bir sayı giriniz!");
             }
         }
     }
 }
+    
+

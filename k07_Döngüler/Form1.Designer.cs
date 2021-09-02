7de22a9eeb77b1f6353238657392b971fbd03d27
leyla <leyla0694@outlook.com>
2021-09-02 14:01:09 +0300

    döngüler

---
+++ k07_Döngüler/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace k07_Döngüler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IstSonuc = new System.Windows.Forms.ListBox();
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.btnOrnek2 = new System.Windows.Forms.Button();
+            this.btnOrnek3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnOrnek6 = new System.Windows.Forms.Button();
+            this.btnOrnek7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IstSonuc
+            // 
+            this.IstSonuc.FormattingEnabled = true;
+            this.IstSonuc.ItemHeight = 16;
+            this.IstSonuc.Location = new System.Drawing.Point(383, 34);
+            this.IstSonuc.Name = "IstSonuc";
+            this.IstSonuc.Size = new System.Drawing.Size(172, 388);
+            this.IstSonuc.TabIndex = 0;
+            this.IstSonuc.SelectedIndexChanged += new System.EventHandler(this.IstSonuc_SelectedIndexChanged);
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Location = new System.Drawing.Point(37, 34);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(296, 41);
+            this.btnOrnek1.TabIndex = 1;
+            this.btnOrnek1.Text = "1.1\'den 100\'e kadar sayıları ekle";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.btnOrnek1_Click);
+            // 
+            // btnOrnek2
+            // 
+            this.btnOrnek2.Location = new System.Drawing.Point(37, 81);
+            this.btnOrnek2.Name = "btnOrnek2";
+            this.btnOrnek2.Size = new System.Drawing.Size(296, 45);
+            this.btnOrnek2.TabIndex = 2;
+            this.btnOrnek2.Text = "2.100\'den 1\'e doğru sayıları ekle";
+            this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.btnOrnek2_Click);
+            // 
+            // btnOrnek3
+            // 
+            this.btnOrnek3.Location = new System.Drawing.Point(37, 142);
+            this.btnOrnek3.Name = "btnOrnek3";
+            this.btnOrnek3.Size = new System.Drawing.Size(296, 49);
+            this.btnOrnek3.TabIndex = 3;
+            this.btnOrnek3.Text = "3.10\'dan 50\'ye kadar olan sayıları 3\'er olarak arttırınız";
+            this.btnOrnek3.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Click += new System.EventHandler(this.btnOrnek3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "4.A\'dan Z\'ye kadar olan harfleri yazdırınız";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(296, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "5.20\'den 100\'e kadar ki çift sayıları gösteriniz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnOrnek6
+            // 
+            this.btnOrnek6.Location = new System.Drawing.Point(37, 312);
+            this.btnOrnek6.Name = "btnOrnek6";
+            this.btnOrnek6.Size = new System.Drawing.Size(296, 42);
+            this.btnOrnek6.TabIndex = 6;
+            this.btnOrnek6.Text = "6.1 ile 10 arasındaki sayıların toplamını göteriniz";
+            this.btnOrnek6.UseVisualStyleBackColor = true;
+            this.btnOrnek6.Click += new System.EventHandler(this.btnOrnek6_Click);
+            // 
+            // btnOrnek7
+            // 
+            this.btnOrnek7.Location = new System.Drawing.Point(37, 369);
+            this.btnOrnek7.Name = "btnOrnek7";
+            this.btnOrnek7.Size = new System.Drawing.Size(296, 44);
+            this.btnOrnek7.TabIndex = 7;
+            this.btnOrnek7.Text = "7.10 ile 30 arasındaki tek sayıların toplamını bulunuz.";
+            this.btnOrnek7.UseVisualStyleBackColor = true;
+            this.btnOrnek7.Click += new System.EventHandler(this.btnOrnek7_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "rastgele sayı veriniz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(593, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 52);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "çarpım tablosunu gösterelim";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(618, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(185, 37);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "yan yana 10 tane x yazdır";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(623, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "lbl yazı tahtası";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 529);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnOrnek7);
+            this.Controls.Add(this.btnOrnek6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOrnek3);
+            this.Controls.Add(this.btnOrnek2);
+            this.Controls.Add(this.btnOrnek1);
+            this.Controls.Add(this.IstSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox IstSonuc;
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.Button btnOrnek2;
+        private System.Windows.Forms.Button btnOrnek3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnOrnek6;
+        private System.Windows.Forms.Button btnOrnek7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ k07_Döngüler/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace k07_Döngüler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrnek1_Click(object sender, EventArgs e)
+        {
+            //1'den 100'e kadar sayıları listeye ekleyiniz.
+            //object: tüm veri tiplerini kapsar.
+            for (int i = 1; i <= 100; i++)
+            {
+                IstSonuc.Items.Add(i);
+            }
+
+        }
+
+        private void btnOrnek2_Click(object sender, EventArgs e)
+        {
+            //100'den 1' e kadar olan sayıları listeleyiniz.
+            for (int i = 100; i >= 1; i--)
+            {
+                IstSonuc.Items.Add(1);
+            }
+        }
+
+        private void btnOrnek3_Click(object sender, EventArgs e)
+        {
+            for (int i = 10; i >= 50; i += 3)
+            {
+                IstSonuc.Items.Add(i);
+
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                IstSonuc.Items.Add(i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 20; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    IstSonuc.Items.Add(i);
+                }
+            }
+
+
+
+        }
+
+
+
+        private void btnOrnek6_Click(object sender, EventArgs e)
+        {
+            var toplam = 0;
+            for (int i = 1; i <= 10; i++)
+
+            {
+                toplam = toplam + i;
+            }
+            MessageBox.Show(toplam.ToString());
+        }
+
+        private void btnOrnek7_Click(object sender, EventArgs e)
+        {
+            var toplam = 0;
+            for (int i = 10; i <= 30; i++)
+                if (i % 2 == 1)
+                {
+                    toplam = toplam + i;
+
+                }
+            MessageBox.Show(toplam.ToString());
+        }
+
+        private void IstSonuc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            var rastgelesayi = rnd.Next(1, 11);
+            MessageBox.Show(rastgelesayi.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i<=10 ; i++)
+            {
+                IstSonuc.Items.Add($"{i} Tablosu");
+                for (int j = 1; j<=10 ; j++)
+                {
+                    IstSonuc.Items.Add($"{i}*{j}={i * j}");
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
